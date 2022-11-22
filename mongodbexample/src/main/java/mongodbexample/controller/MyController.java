4160aa46ca0a9cca475106ba0672b2fc8a750352
BRANE-LAB <rakshitrudragoudar139@gmail.com>
2022-11-22 14:38:38 +0530

    mongodb

---
+++ mongodbexample/src/main/java/mongodbexample/controller/MyController.java
@@ -0,0 +1,26 @@
+package mongodbexample.controller;
+
+import mongodbexample.models.Student;
+import mongodbexample.rep.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+public class MyController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping
+    public ResponseEntity<?> addStudent(@RequestBody Student student){
+        Student save = this.studentRepository.save(student);
+        return ResponseEntity.ok(save);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getStudents() {
+        return ResponseEntity.ok(this.studentRepository.findAll());
+    }
+}
